e7d32f8348ad35a2fb434f65eb811a4ba01c74b8
Dan Grossberg <dwgrossberg@gmail.com>
2022-05-02 14:59:57 +0700

    Setup basic TaskFactory logic

--- src/task.js
+++ src/task.js
@@ -1,12 +1,23 @@
-const TaskFactory = (title) => {
+const TaskFactory = (project, title, details, dueDate, priority) => {
     const task = {
-        title : title
-
+        project : project,
+        title : title,
+        details : details,
+        dueDate : dueDate,
+        priority : priority
+    }
+    
+    const editTitle = (title) => {
+        task.title = title;
     }
 
     return {
-        task
+        task,
+        editTitle
     }
 }
-const yoyo = TaskFactory('peaches');
+const yoyo = TaskFactory('home', 'peaches', 'eat peaches', 'nov. 2nd', 'very high');
+console.log(yoyo, yoyo.task.title)
+
+yoyo.editTitle('piss');
 console.log(yoyo, yoyo.task.title);
