7ab8fc7060efbdcce80511fcd769cb49c8f1f45e
Dan Grossberg <dwgrossberg@gmail.com>
2022-05-04 17:43:44 +0700

    Setup createTask method to function with Project varible names, as opposed to strings

--- src/taskMaster.js
+++ src/taskMaster.js
@@ -23,26 +23,29 @@ const taskMaster = (() => {
     // Create new Tasks and push them to the taskList
     const createTask = (...args) => {
         const newTask = Task(...args);
-        taskList.push(newTask.task);
-        console.log(newTask.task.project);
-        home.addTask(newTask.task)
-        // newTask.task.project.addTask(newTask.task);
+        taskList.push(newTask.task); //Keep track of all Tasks in the taskList array
+        if (args[0] !== home) {
+            home.addTask(newTask.task);
+        } //Add all Tasks to the Home Project by default if they are not already so
+        args[0].addTask(newTask.task); //Automatically add the new Task to the correct Project
+        // let propName = getOwnPropertyNames();
+        // console.log(args[0]);   //.addTask(newTask.task);
 
         return newTask;
     }
 
     // Default tasks on page load
-    const runTask = createTask('home', 'Run 10k practice pace for race', '10.2.22', 'Low', '', false, false);
-    const studyTask = createTask('next7Days', 'Review Webpack.config.js configuration basics', '5/20/2022', 'medium', 'Revist the Webpack guides page and review relevant info', ['Asset Managment', 'Output', 'Development'], false);
-    const funTask = createTask('today', 'Meet up with Lou for a beer', '5/3/22', 'high', 'Meet at Jax Brewery near 9th street', '', false);
-    const emptyTask = createTask();
+    const runTask = createTask(home, 'Run 10k practice pace for race', '10.2.22', 'Low', '', false, false);
+    const studyTask = createTask(next7Days, 'Review Webpack.config.js configuration basics', '5/20/2022', 'medium', 'Revist the Webpack guides page and review relevant info', ['Asset Managment', 'Output', 'Development'], false);
+    const funTask = createTask(today, 'Meet up with Lou for a beer', '5/3/22', 'high', 'Meet at Jax Brewery near 9th street', '', false);
+    const emptyTask = createTask(home);
 
     // Add default Tasks to designated Projects
     // taskList.forEach(task => home.addTask(task));
     // today.addTask(funTask.task);
     // next7Days.addTask(studyTask.task);
 
-    // Modify the taskList to be ordered by date
+    // Sort the taskList so that it is ordered by date
     const dateOrderTaskList = (taskList) => {
         taskList.sort(function(a, b) {
             if (a.dueDate < b.dueDate) return -1;
