d23bc644c0e7603f8b261e3cbc0ee74835f0f66d
Dan Grossberg <dwgrossberg@gmail.com>
2022-05-14 20:55:07 +0700

    Setup ui tabController function to handle DOM tasks related to the sidebar tab

--- src/ui.js
+++ src/ui.js
@@ -15,6 +15,43 @@ const displayUI = (() => {
         }
     }
 
+    // Check if a date is today
+    const isToday = (date) => {
+        if (date) {
+            const today = new Date();
+            return date.getDate() == today.getDate() &&
+                date.getMonth() == today.getMonth() &&
+                date.getFullYear() == today.getFullYear();
+        }
+    }
+    
+    // Check if a date is within the next 7 days
+    const isNextWeek = (date) => {
+        if (date) {
+            const today = new Date();
+            const nextWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 7);
+            if (nextWeek < date) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+
+    const tabController = (taskProject) => {
+        // Display to the updated project list, unless the user is already on Home / Today /Next7Days tab
+        const project = document.getElementById(`Project-${taskProject}`);
+        if (home.style.color === "rgb(216, 39, 117)") return;
+        else if (today.style.color === "rgb(216, 39, 117)" && document.createEvent) {
+            today.dispatchEvent(new Event('mousedown'));
+        } else if (next7Days.style.color ===  "rgb(216, 39, 117)" && document.createEvent) {
+            next7Days.dispatchEvent(new Event('mousedown'));
+        }
+        else if (document.createEvent) {
+            project.dispatchEvent(new Event('mousedown'));
+        }
+    }
+
     const updateTaskCompleteStatus = () => {
         const taskCheckboxes = Array.from(document.querySelectorAll('[id^="task-checkbox-"]'));
         taskCheckboxes.forEach(checkbox => checkbox.addEventListener('change', (e) => {
@@ -22,7 +59,6 @@ const displayUI = (() => {
             // Find the index of the Task object with the matching title
             const taskIndex = taskMaster.taskList.findIndex(task => task.task.title === taskTitle);
             const taskProject = taskMaster.taskList[taskIndex].task.project;
-            const project = document.getElementById(`Project-${taskProject}`);
             console.log(taskMaster.taskList[taskIndex].task);
             if (e.target.checked) {                
                 taskMaster.taskList[taskIndex].changeCompleteStatus(true);
@@ -39,16 +75,7 @@ const displayUI = (() => {
                 loadTaskCards.run(taskMaster.taskList);
                 runDOMTaskFunctions();
             }
-            // Display to the updated project list, unless the user is already on Home / Today /Next7Days tab
-            if (home.style.color === "rgb(216, 39, 117)") return;
-            else if (today.style.color === "rgb(216, 39, 117)" && document.createEvent) {
-                today.dispatchEvent(new Event('mousedown'));
-            } else if (next7Days.style.color ===  "rgb(216, 39, 117)" && document.createEvent) {
-                next7Days.dispatchEvent(new Event('mousedown'));
-            }
-            else if (document.createEvent) {
-                project.dispatchEvent(new Event('mousedown'));
-            }
+            tabController(taskProject);
         }));
     }
 
@@ -60,37 +87,12 @@ const displayUI = (() => {
             const taskTitle = e.target.parentNode.parentNode.childNodes[1].innerText;
             // Find the index of the Task object with the matching title
             const taskIndex = taskMaster.taskList.findIndex(task => task.task.title === taskTitle);
+            const taskProject = taskMaster.taskList[taskIndex].task.project;
             taskMaster.taskList[taskIndex].changeProject(selectedOption);
             console.log(taskMaster.taskList[taskIndex].task);
             // Display the updated project list
-            const project = document.getElementById(`Project-${selectedOption}`);            
-            if (document.createEvent) {
-                project.dispatchEvent(new Event('mousedown'));
-            }
+            tabController(taskProject);
         }));
-    }
-
-    // Check to find Task dueDates that match today
-    const isToday = (date) => {
-        if (date) {
-            const today = new Date();
-            return date.getDate() == today.getDate() &&
-                date.getMonth() == today.getMonth() &&
-                date.getFullYear() == today.getFullYear();
-        }
-    }
-
-    // Check to find Task dueDates that match next7Days
-    const isNextWeek = (date) => {
-        if (date) {
-            const today = new Date();
-            const nextWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 7);
-            if (nextWeek < date) {
-                return false;
-            } else {
-                return true;
-            }
-        }
     }
 
     const updateTaskDueDate = () => {
@@ -114,17 +116,7 @@ const displayUI = (() => {
             // Re-attach event listener functions to Task DOM objects
             runDOMTaskFunctions();
             // Display to the updated project list, unless the user is already on Home / Today /Next7Days tab
-            console.log(taskProject);
-            const project = document.getElementById(`Project-${taskProject}`);
-            if (home.style.color === "rgb(216, 39, 117)") return;
-            else if (today.style.color === "rgb(216, 39, 117)" && document.createEvent) {
-                today.dispatchEvent(new Event('mousedown'));
-            } else if (next7Days.style.color ===  "rgb(216, 39, 117)" && document.createEvent) {
-                next7Days.dispatchEvent(new Event('mousedown'));
-            }
-            else if (document.createEvent) {
-                project.dispatchEvent(new Event('mousedown'));
-            }
+            tabController(taskProject);
             }));
     }
 
@@ -198,7 +190,7 @@ const displayUI = (() => {
             e.target.parentNode.parentNode.remove();
             // Remove the Task object from the taskMasker.taskList
             taskMaster.removeTask(taskIndex);
-            console.log(taskMaster.taskList)
+            console.log(taskMaster.taskList);
         }));
     }
     
@@ -211,7 +203,7 @@ const displayUI = (() => {
         deleteTask();
     }
 
-    const updateSidebarCounters = () => {
+    const setSidebarCounters = () => {
         // Set Project sidebar counters for dynamic use
         const projectCounter = document.getElementById('projects-counter');
         projectCounter.innerText = taskMaster.projectList.length - 1; //Subtract one to account for Home as default Project
@@ -219,24 +211,26 @@ const displayUI = (() => {
             let counterElem = document.getElementById(`project-counter-${project.project.name}`);
             counterElem.innerText = taskMaster.projectList[taskMaster.projectList.indexOf(project)].project.tasks.length;
         });
-
         // Set Today and Next7Days counters
         const todayCounter = document.getElementById('today-counter');
         const next7DaysCounter = document.getElementById('next-seven-days-counter');
         taskMaster.taskList.forEach(task => {
             if (isToday(task.task.dueDate)) {
-                todayCounter.innerText += 1;
-            } else if (isNextWeek(task.task.dueDate)) {
-                next7DaysCounter.innerText += 1;
+                todayCounter.innerText++;
+            } 
+            if (isNextWeek(task.task.dueDate)) {
+                next7DaysCounter.innerText++;
             }
         });
-        
-
-        // todayCounter.innerText = todayTasks.length;
-        // next7DaysCounter.innerText = next7DaysTasks.length;
     }
 
-    updateSidebarCounters();
+    const addSidebarCounter = () => {
+
+    }
+
+    const deleteSidebarCounter = () => {
+
+    }
 
     const displayController = () => {
 
@@ -245,6 +239,7 @@ const displayUI = (() => {
         home.style.fontWeight = 'bold';
         loadTaskCards.run(taskMaster.taskList);
         runDOMTaskFunctions();
+        setSidebarCounters();
 
         // Home
         home.addEventListener('mousedown', (e) => {
